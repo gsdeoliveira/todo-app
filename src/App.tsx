f40c188063b3ce54c20b87c13be3e48aeb5596f2
gsdeoliveira <gabriielsousa2002@gmail.com>
2025-02-16 19:40:40 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeToggle } from "./components/theme-toggle"
+
 function App() {
   return (
-    <div>
+    <div className="w-full h-screen font-josefin text-lg bg-background">
+      <div className="w-96 h-96 bg-muted">
+        <p className="text-teste">Teste todo</p>
+        <p className="text-teste2">Teste todo</p>
+        <p>Teste todo</p>
+      </div>
       <h1 className="text-4xl text-violet-500">Hello World!</h1>
+      <ThemeToggle />
     </div>
   )
 }
